d835fcc4e5134658484b66bf161ef4a9516bba4c
Liviru <119770778+livfy@users.noreply.github.com>
2024-11-04 16:04:54 +1100

    Implement predicting next word

--- Sources/MarkovTextGenerator/MarkovTextGenerator.swift
+++ Sources/MarkovTextGenerator/MarkovTextGenerator.swift
@@ -1,2 +1,38 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+let START = "START"
+let END = "END"
+
+typealias markovChain = [String: [String]]
+
+func buildMarkovChain(from textSource: String) -> markovChain {
+    var chain: markovChain = [:]
+    let tokenizedText = textSource.lowercased().split(separator: "\n").map {
+        $0.split(separator: " ").map {
+            String($0)
+        }
+    }
+
+    for sentence in tokenizedText {
+        let markedSentence = [START] + sentence + [END]
+        for index in 0...sentence.count {
+            let state = markedSentence[index]
+            let next = markedSentence[index + 1]
+
+            chain[state, default: []].append(next)
+        }
+    }
+
+    return chain
+}
+
+class TextGenerator {
+    let chain: markovChain
+
+    init(fromTextSource textSource: String) {
+        chain = buildMarkovChain(from: textSource)
+    }
+
+    func predictNextWord(fromWord word: String) -> String? {
+        let nextStates = chain[word]
+        return nextStates?.randomElement()
+    }
+}
--- Tests/MarkovTextGeneratorTests/MarkovTextGeneratorTests.swift
+++ Tests/MarkovTextGeneratorTests/MarkovTextGeneratorTests.swift
@@ -1,6 +1,16 @@
 import Testing
+
 @testable import MarkovTextGenerator
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func nextWord() async throws {
+    let text = """
+        The cat ran down some stairs.
+        A mouse sprinted to get away from the cat.
+        My cat wanted some milk.
+        A cat watched patiently.
+        """
+
+    let generator = TextGenerator(fromTextSource: text)
+    let predictedWord = generator.predictNextWord(fromWord: "mouse")
+    #expect(predictedWord == "sprinted")
 }
