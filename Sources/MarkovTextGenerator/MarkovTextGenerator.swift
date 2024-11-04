f06be13659f4f96a7cb21b197403cb8e778830d2
Liviru <119770778+livfy@users.noreply.github.com>
2024-11-05 09:25:55 +1100

    Implement creating sentences

--- Sources/MarkovTextGenerator/MarkovTextGenerator.swift
+++ Sources/MarkovTextGenerator/MarkovTextGenerator.swift
@@ -1,38 +1,44 @@
 let START = "START"
-let END = "END"
 
-typealias markovChain = [String: [String]]
-
-func buildMarkovChain(from textSource: String) -> markovChain {
-    var chain: markovChain = [:]
-    let tokenizedText = textSource.lowercased().split(separator: "\n").map {
-        $0.split(separator: " ").map {
-            String($0)
-        }
-    }
-
-    for sentence in tokenizedText {
-        let markedSentence = [START] + sentence + [END]
-        for index in 0...sentence.count {
-            let state = markedSentence[index]
-            let next = markedSentence[index + 1]
-
-            chain[state, default: []].append(next)
-        }
-    }
-
-    return chain
-}
+typealias transitionMatrix = [String: [String]]
 
 class TextGenerator {
-    let chain: markovChain
+    var transitions: transitionMatrix = [:]
 
     init(fromTextSource textSource: String) {
-        chain = buildMarkovChain(from: textSource)
+        let tokenizedText = textSource.split(separator: "\n").map {
+            $0.split(separator: " ").map {
+                String($0).lowercased()
+            }
+        }
+
+        for sentence in tokenizedText {
+            let markedSentence = [START] + sentence
+            for index in 0..<sentence.count {
+                let state = markedSentence[index]
+                let next = markedSentence[index + 1]
+
+                transitions[state, default: []].append(next)
+            }
+        }
     }
 
     func predictNextWord(fromWord word: String) -> String? {
-        let nextStates = chain[word]
+        let nextStates = transitions[word]
         return nextStates?.randomElement()
+    }
+
+    func makeSentence() -> String? {
+        var state = START
+        var sentence = ""
+
+        while true {
+            guard let nextWord = predictNextWord(fromWord: state) else {
+                return sentence.isEmpty ? nil : sentence
+            }
+
+            sentence.append(contentsOf: " " + nextWord)
+            state = nextWord
+        }
     }
 }
--- Tests/MarkovTextGeneratorTests/MarkovTextGeneratorTests.swift
+++ Tests/MarkovTextGeneratorTests/MarkovTextGeneratorTests.swift
@@ -14,3 +14,16 @@ import Testing
     let predictedWord = generator.predictNextWord(fromWord: "mouse")
     #expect(predictedWord == "sprinted")
 }
+
+@Test func makeSentence() async throws {
+    let text = """
+        The cat ran down some stairs.
+        A mouse sprinted to get away from the cat.
+        My cat wanted some milk.
+        A cat watched patiently.
+        """
+
+    let generator = TextGenerator(fromTextSource: text)
+    let sentence = generator.makeSentence()
+    #expect(sentence != nil)
+}
